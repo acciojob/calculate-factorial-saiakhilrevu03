e810b523dc381329221a92b11c67247bbded852e
saiakhilrevu03 <102013188+saiakhilrevu03@users.noreply.github.com>
2025-07-30 01:07:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+function factorial(n){
+	let fact=1;
+	for(let i=n;i>=1;i--){
+		fact=fact*i;
+	}
+	return `The factorial of ${n} is ${fact}`;
+}
+const n = parseInt(prompt("enter a number"));
+if(!isNaN(n) && n>=0){
+	alert(factorial(n));
+}
